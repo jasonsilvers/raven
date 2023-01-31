78167caa223860db9a0b6781072f08cb20f94d40
Jason Silvers <jason.silvers.2@us.af.mil>
2023-01-30 14:54:42 -1000

    remove api directory

--- apps/militaryresume/remix.config.js
+++ apps/militaryresume/remix.config.js
@@ -3,10 +3,6 @@
  */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
   watchPaths: ['../../libs'],
   server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
   serverBuildTarget: 'vercel',
